7cf03efbbd6e10b5ae4590eb6397aef778c069cd
Ryan <rngrill5@gmail.com>
2024-11-26 16:39:43 -0600

    beginning search development, reformating display

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -7,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+//Register the Firebase Database
+builder.Services.AddScoped<FirebaseService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
